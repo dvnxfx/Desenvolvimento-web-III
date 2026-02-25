0e5d52127578454e9c36031c0d441cb0a76af049
DSM3 <fatec-dsm3@academico.intra>
2026-02-25 19:17:32 -0300

    aula-01-criando-a-api

---
+++ aula-01-criando-a-api/models/Games.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const gameSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    platform: String,
+    price:  Number
+});
+
+const Game = mongoose.model('Game', gameSchema)
+
+export default Game;
